4ad6aec6c5aec5d5824660ad3c64d56a1c4df392
Sanne 't Hooft <shooft@me.com>
2025-02-05 23:53:41 +0100

    endpoint met avatar

--- scripts/myName.js
+++ scripts/myName.js
@@ -1,7 +1,7 @@
 // api documentation: https://whois.fdnd.nl/docs/
 
 // 1 persoon: https://fdnd.directus.app/items/person/67
-// iedereen: https://fdnd.directus.app/items/person/?fields=id,name,github_handle&filter={%22squads%22:{%22squad_id%22:{%22name%22:%22Minor%20Web%20Dev%22}}}&sort=name
+// iedereen: https://fdnd.directus.app/items/person/?fields=id,name,github_handle,avatar&filter={%22squads%22:{%22squad_id%22:{%22name%22:%22Minor%20Web%20Dev%22}}}&sort=name
 
 const baseURL = 'https://fdnd.directus.app/';
 const endpointMe = 'items/person/67';
